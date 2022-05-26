a0ae5b196a5e3a88371e61b640612adaa3d67500
uallau <94310253+uallau@users.noreply.github.com>
2022-05-26 18:44:16 +0800

    Add files via upload

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,10 +42,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django-filters',
+    'storages',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
